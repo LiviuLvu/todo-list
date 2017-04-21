4e6bd2603af536bc37a4621ccfb9fa859b7347bb
Liviu Iancu <liviuiancu@gmx.com>
2017-04-21 18:30:48 +0300

    todo app with mlab database and node.js

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,23 +1,42 @@
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
-var data = [{item: 'bach'}, {item: 'straus'}, {item: 'vivaldi'}];
+// Connect to mlab database
+mongoose.connect('mongodb://lvu:8Evh&IsdLh5J@ds135069.mlab.com:35069/todo-lvu');
+
+// create database schema
+var todoSchema = new mongoose.Schema({
+  item: String
+});
+
+var Todo = mongoose.model('Todo', todoSchema);
+
+// var data = [{item: 'bach'}, {item: 'straus'}, {item: 'vivaldi'}];
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 
 module.exports = function (app) {
+
   app.get('/todo', function (req, res) {
-    res.render('todo', {todos: data});
+    // get data from mongo db, pass to view
+    Todo.find({}, function (err, data) {
+      if (err) throw err;
+      res.render('todo', {todos: data});
+    });
   });
   
   app.post('/todo', urlencodedParser, function (req, res) {
-    data.push(req.body);
-    res.render('todo', {todos: data});
-    res.json(data);
+    // get data from view, add to mongo db
+    var newTodo = Todo(req.body).save(function (err, data) {
+      if (err) throw err;
+      res.json(data);
+    });
   });
 
   app.delete('/todo/:item', function (req, res) {
-    data = data.filter(function (todo) {
-      return todo.item.replace(/ /g, '-') !== req.params.item;
+    // delete the requested item from mongo db
+    Todo.find({item: req.params.item.replace(/\-/g, " ")}).remove(function (err, data) {
+      if (err) throw err;
+      res.json(data);
     });
-    res.json(data);
   });
 };
